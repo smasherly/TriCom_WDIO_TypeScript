09fa83384d408b37a453c62cd594ee0f6c80a84b
Smasherly <ashholbrook@gmail.com>
2020-03-03 18:49:03 -0600

    fixed a few things

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # WebdriverIO_v5_TypeScript
 
-This is a boilerplate project that uses WebdriverIO v5 and TypeScript v3. 
+This project that uses WebdriverIO v5 and TypeScript v3. It runs a couple tests on the contact us form on a publicly available site.
 
 ## Getting Started
-```
-git clone git@github.com:jpolley/WebdriverIO_v5_TypeScript.git
-cd WebdriverIO_v5_TypeScript
+
 npm install
 npm test
 ```
--- test/contactUs.ts
+++ test/contactUs.ts
@@ -14,20 +14,16 @@ describe("required fields on contact us form", () => {
     it("checks that page name is Contact Us", () => {
         expect(ContactUs.h1).to.eq("Contact Us")
     });
-    //TODO Submit and see all fields that are required
     it("clicks submit to activate error messages", () => {
         ContactUs.submit.click();
     });
     it("it checks that error messages show", () => {
         expect(ContactUs.yourNameError.getText()).to.eq("The field is required.")
-        console.log(ContactUs.yourPhoneError)
         expect(ContactUs.yourPhoneError).to.eq("The field is required.")
         expect(ContactUs.yourEmailError).to.eq("The field is required.")
         expect(ContactUs.yourMessageError).to.eq("The field is required.")
         expect(ContactUs.theFormError.getText()).to.eq("One or more fields have an error. Please check and try again.")
-
         //Submits and sucess message?
         //clearValue()
-
     });
 });
